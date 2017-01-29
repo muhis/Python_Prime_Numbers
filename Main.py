a4cb1b22410cb9f038cf9fee1d8de12b6d80fbbb
= <m.salman@zinkki.com>
2017-01-29 18:33:45 +0200

    Added a second method to the functions. now there is a third argument to the function, the argument is numerical so: (1) Is the old method, not using any list and direct calculation. (2) using Sieve and lists, though it does not work if you start from number 1.

--- Main.py
+++ Main.py
@@ -1,15 +1,18 @@
-First_Number = 10
-Second_Number = 100
-Is_It_Prime = True
-Prime_Numbers = []
-for i in range (First_Number,Second_Number):
-	Is_It_Prime = True
-	for j in range (2,i):
-		if (i%j==0):
-			Is_It_Prime = False
-			break
-		Is_It_Prime = True
-	if (Is_It_Prime):
-		Prime_Numbers.append (i)
-
-print (Prime_Numbers)
+def primes(first_number, second_number,method):
+	if(method == 1):
+		Prime_Numbers = []
+		for i in range (first_number,second_number):
+			is_it_prime = True
+			for j in range (2,i):
+				if (i%j==0):
+					is_it_prime = False
+					break
+				is_it_prime = True
+			if (is_it_prime):
+				Prime_Numbers.append (i)
+		return Prime_Numbers
+	else:
+		notprime = [j for i in range(first_number,8) for j in range(i*2,second_number,i)]
+		prime = [i for i in range(first_number,second_number) if i not in notprime]
+		return(prime)
+print(primes(2,100,2))
